582e9a7f573d4c0a15d0960f52327272b494efa9
Romario <hyperromario@yahoo.com>
2017-04-21 11:49:47 +0300

    Last version with comments

--- js/calendar.js
+++ js/calendar.js
@@ -1,37 +1,36 @@
-var monthLiteral = null,
+var monthLiteral = null,// Global variables for content
     curYear = null;
-
 
 function createCalendar(id, year, month) {
     var calendar = document.getElementById('calendar'),
         monthList = ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December'],
-        curMonth = month - 1,
+        curMonth = month - 1, // months in JS go from 0 to 11, not from 1 to 12
         curDate = new Date(year, curMonth),
         table = '<table><tr><td id="calendarHeader" colspan="7"></td></tr><tr>';
     table = table + '<th>Mon</th><th>Tue</th><th>Wed</th><th>Thu</th><th>Fri</th><th>Sat</th><th>Sun</th></tr><tr>';
     monthLiteral = monthList[curMonth];
     curYear = year;
 
-    for (var i = 0; i < getDay(curDate); i++) {
+    for (var i = 0; i < getDay(curDate); i++) {  //Fill the first row from Monday and until the day the month// begins:// * * * | 1 2 3 4
         table = table + '<td></td>';
     }
 
-    while (curDate.getMonth() == curMonth) {
+    while (curDate.getMonth() == curMonth) { //Fill in calendar cells with dates
         table = table + '<td>' + curDate.getDate() + '</td>';
 
-        if (getDay(curDate) % 7 == 6) {
+        if (getDay(curDate) % 7 == 6) { //Sunday is the last day - line feed
             table = table + '</tr><tr>';
         }
         curDate.setDate(curDate.getDate() + 1);
     }
 
-    if (getDay(curDate) != 0) {
+    if (getDay(curDate) != 0) { // finish the table with empty cells if needed
         for (var i = getDay(curDate); i < 7; i++) {
             table = table + '<td></td>';
         }
     }
 
-    table = table + '</tr></table>';
+    table = table + '</tr></table>'; //close the table
     calendar.innerHTML = table;
 }
 
@@ -41,13 +40,13 @@ function getDay(date) {
     return day - 1;
 }
 
-createCalendar('calendar', 2018, 1);
+createCalendar('calendar', 1983, 6);
 
-var header = document.getElementById('calendarHeader');
+var header = document.getElementById('calendarHeader'); //Content of calendar header
 console.dir(header);
 header.innerText = monthLiteral + ' ' + curYear;
 
-var cells = document.getElementsByTagName('td');
+var cells = document.getElementsByTagName('td'); // Paint weekend days on red
 for (var i = 0; i < cells.length; i++) {
     if (cells[i].cellIndex == 6 || cells[i].cellIndex == 5) {
         cells[i].style.color = 'red';
@@ -59,8 +58,7 @@ var wrapper = document.getElementById('wrapper'),
     close = document.getElementById('close'),
     modal = document.getElementById('curModal');
 
-btn[0].onclick = function (e) {
-    var th = document.getElementsByTagName('th');
+btn[0].onclick = function (e) { // Events on click
     if (e.target.innerText && e.target != header && e.target.nodeName != 'TH') {
         wrapper.style.display = 'block';
         modal.innerText = monthLiteral + ' ' + e.target.innerText + ', ' + curYear;
